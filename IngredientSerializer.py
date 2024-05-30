d10805fe5ca20e09e6cb60b94253d45f4231cf93
swapno963 <swapno963@gmail.com>
2024-05-30 22:19:33 +0600

    a

--- IngredientSerializer.py
+++ IngredientSerializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Ingredients
 
-class UserSerializer(serializers.ModelSerializer):
+class IngredientsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredients
         fields = '__all__'
--- viws.py
+++ viws.py
@@ -1,8 +1,29 @@
 
-from .models import User
+from .models import User,Recipe,Rating,Comment,Ingredients
 from .userSerializer import userSerializer
+from .RecipeSerializer import RecipeSerializer
+from .RatingSerializer import RatingSerializer
+from .CommentSerializer import CommentSerializer
+from .IngredientsSerializer import IngredientsSerializer
+
 from rest_framework import viewsets
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = userSerializer
+    
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class =RecipeSerializer
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+class IngradientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredients.objects.all()
+    serializer_class = IngredientsSerializer
